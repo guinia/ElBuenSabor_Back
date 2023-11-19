e457603063e0e8e016255d5d4a7b4fab15993888
Tomás Mezzabotta <tomasmezzabotta06@gmail.com>
2023-11-19 12:42:30 -0300

    Build

--- src/main/java/com/tup/buensabor/security/Config/ApplicationConfig.java
+++ src/main/java/com/tup/buensabor/security/Config/ApplicationConfig.java
@@ -49,4 +49,21 @@ public class ApplicationConfig {
                 .orElseThrow(()-> new UsernameNotFoundException("Usuario no encontrado"));
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
 }
